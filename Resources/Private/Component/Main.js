8269b4369a4d7836e1ae8e74fe3d8b9cac45e3e5
Jon Uhlmann <me@jonnitto.ch>
2017-11-08 09:56:28 +0100

    TASK: Change kind of how the assets get included

--- Resources/Private/Component/Main.js
+++ Resources/Private/Component/Main.js
@@ -1,66 +1,66 @@
 (function() {
-	var daysToExpire = 3650;
-	var cookieName = 'cookiesAllowed';
-	var visibleClass = '-visible';
-	var dialog = document.querySelector('.cookie-dialogue');
-	var CookieDialogue = {
-		// check for cookie
-		checkCookie: function () {
-			if (CookieDialogue.getCookie() != '1') {
-				setTimeout(function () {
-					dialog.querySelector('.accept').onclick = CookieDialogue.accept;
-					dialog.classList.add(visibleClass);
-				}, 500);
-			} else {
-				CookieDialogue.remove();
-			}
-		},
-		// remove dialog box
-		remove: function () {
-			dialog.parentElement.removeChild(dialog);
-		},
+    var daysToExpire = 3650;
+    var cookieName = "cookiesAllowed";
+    var visibleClass = "-visible";
+    var dialog = document.querySelector(".cookie-dialogue");
+    var CookieDialogue = {
+        // check for cookie
+        checkCookie: function() {
+            if (CookieDialogue.getCookie() != "1") {
+                setTimeout(function() {
+                    dialog.querySelector(".accept").onclick =
+                        CookieDialogue.accept;
+                    dialog.classList.add(visibleClass);
+                }, 500);
+            } else {
+                CookieDialogue.remove();
+            }
+        },
+        // remove dialog box
+        remove: function() {
+            dialog.parentElement.removeChild(dialog);
+        },
 
-		// accept cookie
-		accept: function () {
-			dialog.classList.remove(visibleClass);
-			setTimeout(CookieDialogue.remove, 500);
-			CookieDialogue.setCookie();
-			return false;
-		},
-		// set cookie
-		setCookie: function () {
-			try {
-				var date = new Date();
-				var expires = 'expires=';
-				date.setTime(date.getTime() + (daysToExpire * 86400000)); // 24 * 60 * 60 * 1000 = 86400000
-				expires += date.toUTCString();
-				document.cookie = cookieName + '=1;' + expires + ';path=/';
-			} catch (e) {
-			}
-		},
-		// get cookie
-		getCookie: function () {
-			try {
-				var name = cookieName + '=';
-				var decodedCookie = decodeURIComponent(document.cookie);
-				var cookieArray = decodedCookie.split(';');
-				for (var i = 0; i < cookieArray.length; i++) {
-					var c = cookieArray[i];
-					while (c.charAt(0) == ' ') {
-						c = c.substring(1);
-					}
-					if (c.indexOf(name) == 0) {
-						return c.substring(name.length, c.length);
-					}
-				}
-				return ''
-			} catch (error) {
-				return ''
-			}
-		}
-	};
+        // accept cookie
+        accept: function() {
+            dialog.classList.remove(visibleClass);
+            setTimeout(CookieDialogue.remove, 500);
+            CookieDialogue.setCookie();
+            return false;
+        },
+        // set cookie
+        setCookie: function() {
+            try {
+                var date = new Date();
+                var expires = "expires=";
+                date.setTime(date.getTime() + daysToExpire * 86400000); // 24 * 60 * 60 * 1000 = 86400000
+                expires += date.toUTCString();
+                document.cookie = cookieName + "=1;" + expires + ";path=/";
+            } catch (e) {}
+        },
+        // get cookie
+        getCookie: function() {
+            try {
+                var name = cookieName + "=";
+                var decodedCookie = decodeURIComponent(document.cookie);
+                var cookieArray = decodedCookie.split(";");
+                for (var i = 0; i < cookieArray.length; i++) {
+                    var c = cookieArray[i];
+                    while (c.charAt(0) == " ") {
+                        c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                        return c.substring(name.length, c.length);
+                    }
+                }
+                return "";
+            } catch (error) {
+                return "";
+            }
+        }
+    };
 
-	if (dialog) {
-		CookieDialogue.checkCookie();
-	}
+    if (dialog) {
+        CookieDialogue.checkCookie();
+    }
 })();
